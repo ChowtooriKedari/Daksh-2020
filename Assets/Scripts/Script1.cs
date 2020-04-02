fdde90e70d879179d2af92be7e4fbfdcbfe0470b
Chowtoori Kedari <53082047+ChowtooriKedari@users.noreply.github.com>
2020-04-02 13:24:58 +0530

    Assets

---
+++ Assets/Scripts/Script1.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script1 : MonoBehaviour
+{
+    public TextMesh s;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnCollisionEnter(Collision C)
+    {
+        Debug.Log("Collision Occured");
+        s.text = "Collided With" + C.collider.name;
+    }
+}
