111de967d56bc42038cde010e2c0ba8c4ce872a2
Chowtoori Kedari <53082047+ChowtooriKedari@users.noreply.github.com>
2020-04-02 14:01:08 +0530

    Assets

---
+++ Assets/CS.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class CS : MonoBehaviour
+{
+    float curtime = 0f;
+    float stime = 10f;
+    public Text cdownText;
+    public float text2 = 0f;
+    void Start()
+    {
+        curtime = stime;
+      
+
+        //print(curtime);
+      //  print(stime);
+    }
+    void Update()
+    {
+        curtime -= 1 * Time.deltaTime;
+        cdownText.text = curtime.ToString("0");
+        print(curtime);
+        text2 = curtime;
+        if (curtime <= 0)
+        {
+            curtime = 0;
+             SceneManager.LoadScene("Moon");
+        }
+    }
+
+
+}
---
+++ Assets/Countdown.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Countdown : MonoBehaviour
+{
+    void Start()
+    {
+        // Only specifying the sceneName or sceneBuildIndex will load the Scene with the Single mode
+        SceneManager.LoadScene("Earth", LoadSceneMode.Additive);
+    }
+}
